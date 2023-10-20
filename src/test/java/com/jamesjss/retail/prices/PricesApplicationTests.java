6da2c1610f6ac270d902e955b29400f48478167f
Jaime <jaisan@gmail.com>
2023-10-20 23:52:47 +0200

    feat: Initial Commit

---
+++ src/test/java/com/jamesjss/retail/prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesjss.retail.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
