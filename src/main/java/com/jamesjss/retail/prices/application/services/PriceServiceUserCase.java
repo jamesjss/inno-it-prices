dcdc539242cbe8e43b2ef10760141434672c0a15
Jaime <jaisan@gmail.com>
2023-10-21 23:49:51 +0200

    feat(application): Added UserCase and PricesService

---
+++ src/main/java/com/jamesjss/retail/prices/application/services/PriceServiceUserCase.java
@@ -0,0 +1,11 @@
+package com.jamesjss.retail.prices.application.services;
+
+import com.jamesjss.retail.prices.domain.model.Prices;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+//Input Port
+public interface PriceServiceUserCase {
+    List<Prices> getByDateProductAndBrand(LocalDateTime dateBetween, Long productId, Long brandId);
+}
---
+++ src/main/java/com/jamesjss/retail/prices/application/services/PricesService.java
@@ -0,0 +1,24 @@
+package com.jamesjss.retail.prices.application.services;
+
+import com.jamesjss.retail.prices.application.repository.PricesRepository;
+import com.jamesjss.retail.prices.domain.model.Prices;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PricesService implements PriceServiceUserCase {
+
+    private final PricesRepository pricesRepository;
+
+    public PricesService(PricesRepository pricesRepository) {
+        this.pricesRepository = pricesRepository;
+    }
+
+    @Override
+    public List<Prices> getByDateProductAndBrand(LocalDateTime dateBetween, Long productId, Long brandId) {
+        return pricesRepository.searchByDateProductAndBrand(dateBetween, productId, brandId);
+    }
+
+
+}
+
