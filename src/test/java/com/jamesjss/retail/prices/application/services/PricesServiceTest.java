9ccd81040eed3b5f6b16026c86fcf2ab1a95b1dd
Jaime <jaisan@gmail.com>
2023-10-22 00:02:44 +0200

    test(service): Deleted unused imports

--- src/test/java/com/jamesjss/retail/prices/application/services/PricesServiceTest.java
+++ src/test/java/com/jamesjss/retail/prices/application/services/PricesServiceTest.java
@@ -9,18 +9,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 
