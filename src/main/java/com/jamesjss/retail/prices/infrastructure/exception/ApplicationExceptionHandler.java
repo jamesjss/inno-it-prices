271387bb93c04f35795b220b8b1fb39eb1d9fcb9
Jaime <jaisan@gmail.com>
2023-10-23 19:13:15 +0200

    feature(exception): Added ControllerAdvie for missing request parameters

--- src/main/java/com/jamesjss/retail/prices/infrastructure/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/jamesjss/retail/prices/infrastructure/exception/ApplicationExceptionHandler.java
@@ -4,17 +4,17 @@ import com.jamesjss.retail.prices.application.exception.PriceNotFoundException;
 import com.jamesjss.retail.prices.infrastructure.exception.dto.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 
 @RestControllerAdvice
-public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApplicationExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentTypeMismatchException.class, DateTimeParseException.class})
@@ -28,12 +28,21 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorMessage> handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .statusCode(HttpStatus.BAD_REQUEST)
+                .timestamp(LocalDateTime.now())
+                .message("Missing Servlet Request Parameter")
+                .build();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({PriceNotFoundException.class})
+    @ExceptionHandler(PriceNotFoundException.class)
     public ResponseEntity<Void> handlePriceNotFoundException(PriceNotFoundException ex) {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
-
-
 }
