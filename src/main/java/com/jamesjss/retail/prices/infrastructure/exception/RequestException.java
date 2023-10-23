12f41bb6e8342b3aff3791a03ff979a43575ccaf
Jaime <jaisan@gmail.com>
2023-10-23 15:19:38 +0200

    fix: Class not in use

--- src/main/java/com/jamesjss/retail/prices/infrastructure/exception/RequestException.java
+++
@@ -1,7 +0,0 @@
-package com.jamesjss.retail.prices.infrastructure.exception;
-
-public class RequestException extends Exception {
-    public RequestException(String message) {
-        super(message);
-    }
-}
